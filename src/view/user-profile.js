9eab52b331b9fe8986c5bac031f0fe763a6a6233
PavelKP <mr.pavel.krasnov@yandex.ru>
2020-08-12 08:29:46 +0300

    Испрвлено поведение статуса при 0 фильмов

--- src/view/user-profile.js
+++ src/view/user-profile.js
@@ -1,14 +1,18 @@
 export const createUserProfileTemplate = (filmCards, userProfileData) => {
   // Cards with watched flag
   const watchedFilmsAmount = filmCards.filter((el) => el.isWatched).length;
+  // User profile grade name by default
+  let userGradeName = ``;
 
-  // Count user profile grade
-  const userGrade = Object.entries(userProfileData).filter((grade) => {
-    return watchedFilmsAmount >= grade[1].min && watchedFilmsAmount <= grade[1].max;
-  });
+  if (watchedFilmsAmount !== 0) {
+    // Count user profile grade
+    const userGrade = Object.entries(userProfileData).filter((grade) => {
+      return watchedFilmsAmount >= grade[1].min && watchedFilmsAmount <= grade[1].max;
+    });
 
-  // User profile grade name
-  const userGradeName = userGrade[0][0];
+    // Assign value to user profile grade name
+    userGradeName = userGrade[0][0];
+  }
 
   return (
     `<section class="header__profile profile">
