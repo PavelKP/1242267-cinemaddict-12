703020334a5b0e4355d5bc136a9b7e818b75425f
PavelKP <mr.pavel.krasnov@yandex.ru>
2020-09-29 00:01:18 +0300

    Свойства в снейк кейсе заключены в ковычки

--- src/model/movies.js
+++ src/model/movies.js
@@ -121,31 +121,30 @@ export default class Movies extends Observer {
     return Object.assign(
         {},
         {
-          /* eslint camelcase: ["error", {properties: "never"}]*/
-          id: card.id,
+          'id': card.id,
           comments,
-          film_info: {
-            title: card.title,
-            alternative_title: card.altTitle,
-            total_rating: card.rating,
-            poster: card.poster,
-            age_rating: card.ageRating,
-            director: card.director,
-            writers: card.writers,
-            actors: card.actors,
-            release: {
-              date: releaseDate,
-              release_country: card.country
+          'film_info': {
+            'title': card.title,
+            'alternative_title': card.altTitle,
+            'total_rating': card.rating,
+            'poster': card.poster,
+            'age_rating': card.ageRating,
+            'director': card.director,
+            'writers': card.writers,
+            'actors': card.actors,
+            'release': {
+              'date': releaseDate,
+              'release_country': card.country
             },
-            runtime: card.duration,
-            genre: card.genres,
-            description: card.description
+            'runtime': card.duration,
+            'genre': card.genres,
+            'description': card.description
           },
-          user_details: {
-            watchlist: card.isListed,
-            already_watched: card.isWatched,
-            watching_date: watchingDate,
-            favorite: card.isFavorite,
+          'user_details': {
+            'watchlist': card.isListed,
+            'already_watched': card.isWatched,
+            'watching_date': watchingDate,
+            'favorite': card.isFavorite,
           }
         });
   }
